e93047b8b4c8004b5a026f7e5945fec0f7ac00ba
Toby Crawley <toby@tcrawley.org>
2017-06-19 13:18:32 -0400

    Make test runner more robust

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,17 +6,14 @@ rm -f $semaphore
 java -jar test-server/target/test-server.jar 7001 $semaphore &
 server_pid=$!
 
+function cleanup {
+    kill -9 $server_pid
+}
+trap cleanup EXIT
+
 echo "Waiting for test server to start..."
 while [ ! -f $semaphore ]; do
     sleep 1
 done
 
 swift test
-retval=$?
-
-kill -9 $server_pid
-
-exit $retval
-
-
-

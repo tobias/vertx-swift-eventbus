eacd1050adc0023a543bdd7167835e20f52f8daa
Garri Adrian Nablo <garri.almighty@gmail.com>
2017-07-01 01:39:01 +0800

    Generate iOS project

---
+++ vertx-ios-swift-client/vertx-ios-swift-client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vertx-ios-swift-client
+//
+//  Created by Garri Adrian Nablo on 7/1/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
